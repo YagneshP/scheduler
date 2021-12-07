baf571839ba14b4d076e6801d71bebf0d6be44ae
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-07 17:13:04 -0500

    Refactor Button component by using classname library

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,12 @@
 import React from "react";
-
+import className from "classnames";
 import "components/Button.scss";
 
 
 export default function Button(props) {
-   let buttonClass = "button";
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   }
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
-  
-
+   let buttonClass = className("button", {
+                               " button--confirm" :  props.confirm, 
+                               " button--danger": props.danger 
+                              });
    return <button className={buttonClass} onClick={props.onClick}  disabled={props.disabled}>{props.children}</button>;
 }
