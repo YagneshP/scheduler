741f8f2b3390917f119d6068e36b257849ff777f
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-07 16:54:59 -0500

    Add disabled and clickable to Butoon component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,16 @@ import React from "react";
 
 import "components/Button.scss";
 
+
 export default function Button(props) {
-   return <button>{props.children}</button>;
+   let buttonClass = "button";
+   if (props.confirm) {
+      buttonClass += " button--confirm";
+   }
+   if (props.danger) {
+      buttonClass += " button--danger";
+   }
+  
+
+   return <button className={buttonClass} onClick={props.onClick}  disabled={props.disabled}>{props.children}</button>;
 }
