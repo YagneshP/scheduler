4218287fadc7148257a75f60765412ff44cdedc7
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-07 19:38:43 -0500

    Refactor DayList component by passing array of days and renderning DayyListItem inside it

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,6 +1,15 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList() {
-  return(<ul></ul>);
+export default function DayList(props) {
+  let dayListItems = props.days.map(day => {
+  return( <DayListItem key ={day.id} name = {day.name}
+                 spots = {day.spots} 
+                 selected={day.name === props.day} 
+                 setDay={props.setDay} 
+          />)
+        });
+  return(<ul>
+    {dayListItems}
+  </ul>);
 }
