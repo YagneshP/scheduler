20392d603d168a5e9e4cd38fb0ea087020d72ae4
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-08 17:27:56 -0500

    Passing prop to InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,23 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
-import "/InterviewerList.scss";
+import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
+  const {interviewers} = props;
+  const interviewersList = interviewers.map(interviewer => {
+                                            return (<InterviewerListItem 
+                                                      key={interviewer.id}
+                                                      {...interviewer}
+                                                      selected = {interviewer.id === props.interviewer}
+                                                      setInterviewer = {props.setInterviewer}
+                                                      />)
+                                            })
+
   return(
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        <InterviewerListItem />
+        {interviewersList}
       </ul>
     </section>
   );
