8f08e082f0a7a34f4a9a2236356909ef94394f88
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-08 14:35:12 -0500

    Create InterviewListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function InterviewerListItem() {
+  return(
+    <li className="interviewers__item">
+      <img
+      className="interviewers__item-image"
+      src="https://i.imgur.com/LpaY82x.png"
+      alt="Sylvia Palmer"
+    />
+    Sylvia Palmer
+  </li>
+  );
+}
