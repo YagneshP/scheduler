60d463217542636e6507a0c5a96c349bd44a87d5
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-08 14:31:06 -0500

    Add mock data and place DayList inside of nav tag

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "components/Application.scss";
+import DayList from "./DayList";
 
 export default function Application(props) {
+  const[day, setDay] = useState("Monday");
+
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
   return (
     <main className="layout">
       <section className="sidebar">
@@ -13,15 +33,21 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        
+        <nav className="sidebar__menu">
+          <DayList days={days} day = {day} setDay={setDay}/>
+        </nav>
+       
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
+       
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        
       </section>
     </main>
   );
