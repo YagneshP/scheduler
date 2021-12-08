c3e3056ee032475ec9ea1a183df8f832f727053a
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-08 17:28:39 -0500

    Add stories for InterviewerListItem component

--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerList";
 
 
 /**
@@ -119,3 +120,37 @@ storiesOf("Button", module)
           setInterviewer={action("setInterviewer")}
         />
       ));
+
+      /**
+       * InterviewerList
+       */
+
+       const interviewers = [
+        { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+        { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+        { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+        { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+        { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+      ];
+      
+      storiesOf("InterviewerList", module)
+        .addParameters({
+          backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+        })
+        .add("Initial", () => (
+          <InterviewerList
+            interviewers={interviewers}
+          />
+        ))
+        .add("Selected", () => (
+          <InterviewerList
+            interviewers={interviewers}
+            interviewer={3}
+          />
+        ))
+        .add("Clickable", () => (
+          <InterviewerList
+            interviewers={interviewers}
+            setInterviewer={action("setInterviewer")}
+          />
+        ));
