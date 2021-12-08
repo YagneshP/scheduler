50e2c71e8709da3c0a4ca310d489e4a6af7d1cf6
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-08 14:31:59 -0500

    Remove console.log statement

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,8 +2,11 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+  console.log("props", props);
   let dayListItems = props.days.map(day => {
-  return( <DayListItem key ={day.id} name = {day.name}
+  return( <DayListItem 
+                 key ={day.id} 
+                 name = {day.name}
                  spots = {day.spots} 
                  selected={day.name === props.day} 
                  setDay={props.setDay} 
