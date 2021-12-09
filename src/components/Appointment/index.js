95736c93dc8034fba8d56f4666b8f85ac38bc7c3
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-09 00:47:09 -0500

    Add show and empty component for appointment'

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import 'components/Appointment/styles.scss';
 import Header from './Header';
 import Empty from './Empty';
+import Show from './Show';
 
 export default function Appointment(props) {
+  const {time, interview} = props;
+  
   return (
     <article className='appointment'>
-      {props.time ? `Appointment at ${props.time}` : 'No Appoinments'}
-      <Header />
-      <Empty />
+      <Header time = {time}/>
+      {interview ? <Show student = {interview.student} interviewer = {interview.interviewer}/> : <Empty />}
     </article>
   );
 };
