6d981fa3e2e51ffe75a24dc78696e6b8e4069597
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-08 20:34:15 -0500

    Import Empty.js component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'components/Appointment/styles.scss';
 import Header from './Header';
+import Empty from './Empty';
 
 export default function Appointment(props) {
   return (
     <article className='appointment'>
       {props.time ? `Appointment at ${props.time}` : 'No Appoinments'}
-      <Header/>
+      <Header />
+      <Empty />
     </article>
   );
 };
