6bdc1daa5d6813cc84d6b8d77ce7c448f1332e15
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-08 21:15:29 -0500

    Add stories for Comfirm , Status and Show component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,9 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 /**
  *  Button
@@ -167,3 +170,6 @@ storiesOf("Appoinment", module)
     .add("Appointment with Time", () => <Appointment time="12pm"/>)
     .add("Header", () => <Header time = "12pm" />)
     .add("Empty", () => <Empty onAdd= {action("onAdd")}/>)
+    .add("Show", () => <Show student="Lydia Miller-Jones" interviewer = {interviewer} onEdit = {action("onEdit")} onDelete = {action("onDelete")} />)
+    .add("Confirm", () => <Confirm message = "Delete the appoinment?" onConfirm = {action("onCpnfirm")} onCancel = {action("onCancel")} />)
+    .add("Status", () => <Status message="Deleting"/>)
