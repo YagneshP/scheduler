3cf7a636b46de1ab3c4e847e367df1d652a4a104
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-08 22:56:17 -0500

    Add stories for Form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form"
 
 
 /**
@@ -176,3 +177,5 @@ storiesOf("Appoinment", module)
     .add("Confirm", () => <Confirm message = "Delete the appoinment?" onConfirm = {action("onCpnfirm")} onCancel = {action("onCancel")} />)
     .add("Status", () => <Status message="Deleting"/>)
     .add("Error", () => <Error message="Could not delete appointment" onClose = {action("onClose")} />)
+    .add("Edit", () => <Form student = "Yagnesh" interviewer = {2} interviewers = {interviewers} onSave = {action("onSave")} onCancel = {action("onCancel")} />)
+    .add("Create", () => <Form interviewers = {interviewers} onSave = {action("onSave")} onCancel = {action("onCancel")}/>)
