3d71edbf0624b089af113e5d0468a7762a3939df
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-09 19:22:20 -0500

    Send request to /api/days to get days using useEffect and axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,61 @@
-import React, {useState} from "react";
-
+import React, {useState, useEffect} from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
+import axios from "axios";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 
 export default function Application(props) {
   const[day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get("/api/days")
+         .then(response => {
+           console.log("res", response);
+           setDays(response.data)
+         })
+  }, []);
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+ 
   return (
     <main className="layout">
       <section className="sidebar">
@@ -47,7 +80,10 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        
+        {appointments.map(appointment => (
+          <Appointment key = {appointment.id} {...appointment}/>
+        ))}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
