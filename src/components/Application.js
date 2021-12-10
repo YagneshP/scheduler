b6c97d984b66439533693934563f70de0f2faf69
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-10 18:59:25 -0500

    Used getInterview func to pass interviewer obj

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-import {getAppointmentsForDay} from "../helpers/selectors"
+import {getAppointmentsForDay, getInterview} from "../helpers/selectors"
 export default function Application(props) {
 
   const [state, setState] = useState({day : "Monday", days:[], appointments: {}, interviewers : {} });
@@ -25,7 +25,17 @@ export default function Application(props) {
             })
   }, []);
 
-  
+  const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state,appointment.interview);
+    return (
+      <Appointment 
+        key = {appointment.id} 
+        id = {appointment.id}
+        time = {appointment.time}
+        interview = {interview}
+      />
+    );
+  })
   return (
     <main className="layout">
       <section className="sidebar">
@@ -50,9 +60,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {dailyAppointments.map(appointment => (
-          <Appointment key = {appointment.id} {...appointment}/>
-        ))}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
