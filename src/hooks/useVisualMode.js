201c8fb34ae8d7d210468394836846f9a702a2ed
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-11 14:09:01 -0500

    implement back func to pass test cases

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,28 @@ import { useState } from "react";
 
 const useVisualMode = (intial) => {
   const[mode, setMode] = useState(intial);
+  const[history, setHistory] = useState([intial]);
   const transition = (mode) => {
     setMode(mode);
+    //1.add mode to history array
+    setHistory(prev => [...prev, mode]); 
   }
-  return ({mode, transition});
+  const back = () => {
+    //2. setMode to previouse item of historyArray
+    if(history.length > 1) {
+      setMode(history[history.length - 2]);
+      setHistory(prev => {
+        let newHistory = [...prev];
+        newHistory.pop();
+        return newHistory;
+      });
+    } else {
+      setMode(intial);
+      setHistory([intial]);
+    }
+  
+  }
+  return ({mode, transition, back});
 }
 
 export default  useVisualMode;
