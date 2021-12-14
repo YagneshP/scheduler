2f7f24700f360bf14e5e046929b55d07c694bc26
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-13 21:28:20 -0500

    Add Confirm mode for delete feature

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,30 +5,49 @@ import Empty from './Empty';
 import Show from './Show';
 import useVisualMode from 'hooks/useVisualMode';
 import Form from './Form';
+import Status from './Status';
+import Confirm from './Confirm';
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 export default function Appointment(props) {
-  const {id, time, interview, interviewers, bookInterview} = props;
-  const {mode, transition, back} = useVisualMode( interview ?  SHOW:  EMPTY);
+  const {id, time, interview, interviewers, bookInterview, cancelInterview} = props;
+  const {mode, transition, back} = useVisualMode( interview ?  SHOW :  EMPTY);
  
-  const save = (name, interviewer) => {
+  const save = (name, interviewer) => { 
     const interview = {
       student: name,
       interviewer
     };
     transition(SAVING);
-    bookInterview(id, interview);
-    transition(SHOW);
+    bookInterview(id, interview)
+      .then(() =>transition(SHOW) )
   }
+
+  const confirmCancel = () => {
+    transition(CONFIRM)
+  }
+
+  const cancel = () => {
+    transition(DELETING)
+    cancelInterview(id)
+    .then(() => transition(EMPTY))
+    
+  }
+
   return (
     <article className='appointment'>
       <Header time = {time}/>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE && <Form interviewers = {interviewers} onSave = {save} onCancel = {() => back()}/>}
-      {mode === SHOW && interview && (<Show student = {interview.student} interviewer = {interview.interviewer}/>) }
+      {mode === SAVING && <Status message = "Saving"/> }
+      {mode === CONFIRM && <Confirm message= {'Are you sure you would like to delete?'} onCancel = {() => back()} onConfirm = {cancel}/>}
+      {mode === DELETING && <Status message = "Deleting"/> }
+      {mode === SHOW  && interview && (<Show student = {interview.student} interviewer = {interview.interviewer} onDelete = {confirmCancel}/>) }
     </article>
   );
 };
