8321d645ba94657f141f97fd26e3705629b8a94e
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-15 17:56:22 -0500

    Add data-* for testing

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -17,7 +17,7 @@ export default function DayListItem(props) {
     return `${num} spots remaining`;
   }
   return (
-    <li className = {dayClass}  onClick={()=>props.setDay(props.name)} selected = {props.selected}>
+    <li className = {dayClass} data-testid = "day" onClick={()=>props.setDay(props.name)} selected = {props.selected}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)} </h3>
     </li>
