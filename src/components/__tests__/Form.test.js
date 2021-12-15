063c3a202c8709f67adda3c779a1ad81c78676db
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-14 23:45:59 -0500

    Remove unwanted imports

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, cleanup, fireEvent, getByText} from '@testing-library/react';
+import { render, cleanup, fireEvent} from '@testing-library/react';
 
 import Form from 'components/Appointment/Form';
 
