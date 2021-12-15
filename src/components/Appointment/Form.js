47b7b24bd8ddf8dcbd771ffdade087ef5b2c24cd
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-12-14 23:43:02 -0500

    Add more tests to get more coverage

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -20,6 +20,7 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    setError("");
     props.onSave(student, interviewer)
   }
   return (
